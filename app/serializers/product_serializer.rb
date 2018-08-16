0475ca07433fc587da2c54c67eab7815a556288a
Vanessa Toro <vanetoro84@gmail.com>
2018-08-16 17:30:53 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -7,7 +7,7 @@ class ProductSerializer
 
     serialize_product += '"name": "' + product.name + '" , '
     serialize_product += '"price": "' + product.price.to_s + '" , '
-    serialize_product += '"inventory": ' + product.inventory.to_i + ' , '
+    serialize_product += '"inventory": "' + product.inventory.to_s + '" , '
     serialize_product += '"description": "' + product.description + '"'
 
     serialize_product += "}"
