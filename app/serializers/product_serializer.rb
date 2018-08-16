ebab24f84f36284616f940f20b7e6c9615e2440b
Vanessa Toro <vanetoro84@gmail.com>
2018-08-16 17:06:39 +0000

    Done.

---
+++ app/serializers/product_serializer.rb
@@ -0,0 +1,16 @@
+class ProductSerializer
+
+
+  def self.serialize(product)
+
+    serialize_product  = "{"
+
+    serialize_product += '"name": "' + product.name + '" , '
+    serialize_product += '"price": "' + product.price.to_s + '" , '
+    serialize_product += '"inventory": "' + product.inventory.to_s + '" , '
+    serialize_product += '"description": "' + product.description + '"'
+
+    serialize_product += "}"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
 
-  get '/products/:id/data', to: 'products#product_data'
+  get '/products/:id/product_data', to: 'products#product_data'
 end
